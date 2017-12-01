46b8c58995bcc8220682e420c0ff46c604cb894a
Jedel Ce <Cereceres@users.noreply.github.com>
2017-11-30 20:28:41 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('test to wait module', function () {
         let time1 = Date.now()
         wait.for(3000)
         let time2 = Date.now()
-        assert((time2-time1) >= 1000)
+        assert((time2-time1) >= 3000)
     })
     
     it('shoul block the flow', function (done) {
@@ -15,7 +15,7 @@ describe('test to wait module', function () {
         wait.forAsync(3000)
         .then(function () {
           let time2 = Date.now()
-          assert((time2-time1) >= 1000)
+          assert((time2-time1) >= 3000)
           done()
         })
         
