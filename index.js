7a06ecda915aa29cd3c56c972f9ba40f93b4d902
cereceres <jesus.edelcereceres@gmail.com>
2018-01-04 12:55:38 -0600

    new version is published

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-'use strict';
 module.exports = {
     for(time) {
         const sleep = time + Date.now();
  	    while (Date.now() < sleep) {}
     },
     forAsync(time) {
-            const sleep = time + Date.now();
-            while (Date.now() < sleep) {}
-            return Promise.resolve()
+        const sleep = time + Date.now();
+        while (Date.now() < sleep) {}
+        return Promise.resolve();
     }
 };
